5452e4eb868b1a284c52e5af02cac8da0349255d
Bobby Hayes <154255020+BobbyHayescode@users.noreply.github.com>
2023-12-18 18:23:31 +0200

    Update node viewer.js

--- transport node/node viewer.js
+++ transport node/node viewer.js
@@ -15,29 +15,19 @@
           }
         ],
 
-
+// just a small tissue
   "versions": {
     "0.1.0": {
       "NodeVersion": "",
       "ID": "",
       "Sender": "",
-      "Disabled": false,
+//      "Disabled": false,
       "Name": "good-recipe2",
       "CookbookID": "cb1",
-      "Description": "0.1.0|description of a recipe aaaaa",
+//      "Description": "0.1.0|description of a recipe aaaaa",
       "BlockInterval": 0,
       "ExtraInfo": "",
       "CoinInputs": [
 
 {
-  "creator": "pylo15skmjtnajjg65f30r9ujjz4yfzs9we8zv7uksh",
-  "coinInputs": [
-    {
-      "coins": [
-        {
-          "denom": "testCookbookID/testCoin",
-          "amount": "110"
-        }
-      ]
-    }
-  ]
+  
