2f34c633ca361f178187e2b6b6dc2e0df95a43e9
Bobby Hayes <154255020+BobbyHayescode@users.noreply.github.com>
2023-12-18 18:23:02 +0200

    Create node viewer.js

---
+++ transport node/node viewer.js
@@ -0,0 +1,43 @@
+{
+  "NodeVersion": "0.0.1",
+  "ID": "testTradeId3",
+  "CoinInputs": null,
+  "ItemInputs": [
+    {
+      "CookbookID": "cb1",
+      "ItemInput": {
+        "ID": "",
+        "Doubles": [
+          {
+            "Key": "XP",
+            "MinValue": "1",
+            "MaxValue": "1000000"
+          }
+        ],
+
+
+  "versions": {
+    "0.1.0": {
+      "NodeVersion": "",
+      "ID": "",
+      "Sender": "",
+      "Disabled": false,
+      "Name": "good-recipe2",
+      "CookbookID": "cb1",
+      "Description": "0.1.0|description of a recipe aaaaa",
+      "BlockInterval": 0,
+      "ExtraInfo": "",
+      "CoinInputs": [
+
+{
+  "creator": "pylo15skmjtnajjg65f30r9ujjz4yfzs9we8zv7uksh",
+  "coinInputs": [
+    {
+      "coins": [
+        {
+          "denom": "testCookbookID/testCoin",
+          "amount": "110"
+        }
+      ]
+    }
+  ]
